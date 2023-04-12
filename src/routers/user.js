f536c64fdcc9e7e71e9dc0db26d50eb8801cd61d
huyenduc <duc.phamhuyen@ncc.asia>
2023-04-13 00:21:44 +0700

    đ

--- src/routers/user.js
+++ src/routers/user.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.get('/get-all', userController.getAllUsers);
 router.get('/get-one:id', userController.getUserById);
-router.post('/create',userController.upload.single('avatar'), userController.createUser);
+router.post('/create', userController.createUser);
 router.patch('/update:id', userController.updateUser);
 router.delete('/delete:id', userController.deleteUser);
 
